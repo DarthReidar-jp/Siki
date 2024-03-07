500cdc30c1324f7b414495df34ee27d7e7959160
reira-endo <103876343+reira-endo@users.noreply.github.com>
2024-03-07 02:31:01 +0000

    newpage fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Header from './components/header/header';
 import Display from './components/display/display';
 import Sidebar from './components/sidebar/sidebar';
 import Page from './components/page/Page';
-import NewPage from './components/Newpage/NewPage';
+import NewPage from './components/page/NewPage';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
