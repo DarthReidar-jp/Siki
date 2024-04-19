c61e97592e41c53e66759cc4456a6e4d836203b5
reira-endo <103876343+reira-endo@users.noreply.github.com>
2024-04-19 07:13:07 +0000

    いらないものを削除しました。 book.ts:実装しないため

--- backend/src/utils/promptUtils.ts
+++ backend/src/utils/promptUtils.ts
@@ -2,6 +2,7 @@
 const SystemPrompt = `
     contextに基づいて質問に答えます。口調や言葉遣いをcontextの文章のものに似せてください
     contextに情報がなくわからない場合はユーザーにアドバイスを与えてください
+    過去のチャット履歴に関する事であれば過去のチャットを参照して下さい
     `;
 
 const contentPrompt = `
